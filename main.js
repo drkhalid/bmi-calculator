7bfdbfd6c2f910a7e581afa50fe97ea9c706d958
drkhalid <kh.jamal95@gmail.com>
2018-08-15 01:55:06 +0300

    new feature

--- main.js
+++ main.js
@@ -7,6 +7,14 @@ $('#calculate').on('click', function () {
 
     (e) => myWeight = parseInt(e.target.value, 10);
     (e) => myHeight = parseInt(e.target.value, 10);
-    let myBmi = (parseInt((myWeight / ((myHeight * myHeight) / 10000)), 10))
-    $('#bmi').html(myBmi)
+    let myBmi = ((myWeight / ((myHeight * myHeight) / 10000))).toFixed(2)
+    $('#bmi').html(myBmi);
+    if (myBmi < 18.5) {
+        $('#result').html("You are under weight")
+    } else if (myBmi > 24.9) {
+        $('#result').html("You are over weight")
+    } else {
+        $('#result').html("You have normal weight")
+    }
+
 })
