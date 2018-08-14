def7b5fe8108530fd26ff2108ebe0dc7ea2e510e
drkhalid <kh.jamal95@gmail.com>
2018-08-15 01:23:31 +0300

    my js done

---
+++ main.js
@@ -0,0 +1,12 @@
+let myWeight = 0;
+let myHeight = 0;
+$('#weight').on('keyup', (e) => myWeight = parseInt(e.target.value, 10))
+$('#height').on('keyup', (e) => myHeight = parseInt(e.target.value, 10))
+
+$('#calculate').on('click', function () {
+
+    (e) => myWeight = parseInt(e.target.value, 10);
+    (e) => myHeight = parseInt(e.target.value, 10);
+    let myBmi = (parseInt((myWeight / ((myHeight * myHeight) / 10000)), 10))
+    $('#bmi').html(myBmi)
+})
