ce29eda36668bc37b99c5c9bbad6f611af54f115
drkhalid <kh.jamal95@gmail.com>
2018-08-15 13:48:46 +0300

    first try empty fixed

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 let myWeight = 0;
 let myHeight = 0;
-$('#weight').on('keyup', (e) => myWeight = parseInt(e.target.value, 10))
-$('#height').on('keyup', (e) => myHeight = parseInt(e.target.value, 10))
+$('#weight').on('change', (e) => myWeight = parseInt(e.target.value, 10))
+$('#height').on('change', (e) => myHeight = parseInt(e.target.value, 10))
 
 $('#calculate').on('click', function () {
 
     (e) => myWeight = parseInt(e.target.value, 10);
     (e) => myHeight = parseInt(e.target.value, 10);
-    let myBmi = ((myWeight / ((myHeight * myHeight) / 10000))).toFixed(2)
+    let myBmi = ((myWeight / ((myHeight * myHeight) / 10000)) || 0).toFixed(2)
     $('#bmi').html(myBmi);
-    if (myBmi < 18.5) {
+    if (myBmi == 0.00) {
+        $('#result').html("Please enter your data");
+        $('#bmi').html(' ');
+    } else if (myBmi < 18.5) {
         $('#result').html("You are under weight")
     } else if (myBmi > 24.9) {
         $('#result').html("You are over weight")
